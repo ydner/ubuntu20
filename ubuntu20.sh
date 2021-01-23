0fd78ada98b867cb3342405cbf55f2bac000085f
ydner <76888490+ydner@users.noreply.github.com>
2021-01-23 18:04:09 +0700

    Update ubuntu20.sh

--- ubuntu20.sh
+++ ubuntu20.sh
@@ -1,2 +1,1 @@
 apt update && apt upgrade -y && apt install wget proot git -y && cd ~ && git clone https://github.com/MFDGaming/ubuntu-in-termux.git && cd ubuntu-in-termux && chmod +x ubuntu.sh && ./ubuntu.sh -y
-cd ..
